d9800576ecc0b25f8058cae62ec46693d8787381
cool8612 <looknfeel@nate.com>
2018-11-08 21:58:09 +0900

    클래스 분리
    입력, 출력, 입력파싱,계산로직,메인루프로 분리

---
+++ PRISM/Application/Application/CalculatorReplLoop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class CalculatorReplLoop
+    {
+        public CalculatorReplLoop()
+        {
+            inputService = new ConsoleInputService();
+            outputService = new ConsoleOutputService();
+            calculator = new Calculator();
+            inputParseService = new InputParseService();
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string command = inputService.ReadCommand();
+                try
+                {
+                    CommandTypes commandType = inputParseService.ParseCommand(command);
+
+                    Arguments args = inputService.ReadArguments();
+                    outputService.WriteMessage(
+                        calculator.Execute(commandType, args).ToString());
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+        ConsoleInputService inputService;
+        ConsoleOutputService outputService;
+        Calculator calculator;
+        InputParseService inputParseService;
+
+    }
+}
---
+++ PRISM/Application/Application/ConsoleInputService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class ConsoleInputService
+    {
+        public string ReadCommand()
+        {
+            return Console.ReadLine();
+        }
+
+        public Arguments ReadArguments()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            return new Arguments() { X = x, Y = y };
+        }
+    }
+}
